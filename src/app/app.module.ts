2b7f70feb0bbeef077be3c98aa526c80c1ed7f65
Felipe Castelhano <felipcastelhanogh@gmail.com>
2024-06-12 12:02:34 -0300

    Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
